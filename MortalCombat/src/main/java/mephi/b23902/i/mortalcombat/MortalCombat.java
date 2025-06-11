5a1b67c07a5424979e9f70036e39209c28e72930
kiuttmorpho <tsygankova.elizaveta@gmail.com>
2025-06-11 16:19:26 +0300

    1

---
+++ MortalCombat/src/main/java/mephi/b23902/i/mortalcombat/MortalCombat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b23902.i.mortalcombat;
+
+/**
+ *
+ * @author tsyga
+ */
+public class MortalCombat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
